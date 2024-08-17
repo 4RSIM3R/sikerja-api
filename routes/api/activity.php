ac0e16603f06acd343aea69de977d2af08fe416b
ilzam <ilzammulkhaq85@gmail.com>
2024-08-17 15:42:03 +0700

    feat: otw upload

--- routes/api/activity.php
+++ routes/api/activity.php
@@ -6,9 +6,11 @@ use Illuminate\Support\Facades\Route;
 Route::group(['prefix' => 'activity', 'as' => 'activity.', 'middleware' => ['auth']], function () {
     Route::group(['middleware' => ['custom.permission:admin']], function () {
         Route::delete('{id}', [ActivityController::class, 'destroy'])->name('destroy');
-        Route::resource('', ActivityController::class)->only(['store', 'update']);
+        Route::resource('', ActivityController::class)->only(['store', 'update', 'index', 'show']);
+        Route::get('{id}/export', [ActivityController::class, 'export'])->name('export');
     });
 
-    Route::get('', [ActivityController::class, 'index'])->name('index');
-    Route::get('{id}', [ActivityController::class, 'show'])->name('show');
+    Route::group(['prefix' => 'attendance', 'as' => 'attendance.', 'middleware' => ['custom.permission:user']], function () {
+        Route::post('{id}/evidence', [ActivityController::class, 'evidence'])->name('evidence');
+    });
 });
