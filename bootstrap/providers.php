a5159ae16c76ff68e19786c8f0bf2497967e0e04
ilzam <ilzammulkhaq85@gmail.com>
2024-08-17 10:11:07 +0700

    feat: begin working

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,5 +1,11 @@
 <?php
 
+use Spatie\MediaLibrary\MediaLibraryServiceProvider;
+use Spatie\Permission\PermissionServiceProvider;
+
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\ContractServiceProvider::class,
+    PermissionServiceProvider::class,
+    MediaLibraryServiceProvider::class,
 ];
