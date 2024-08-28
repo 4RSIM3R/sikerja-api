b1e256867dc706a095718963cd1941c79c65b623
ilzam <ilzammulkhaq85@gmail.com>
2024-08-28 12:32:05 +0700

    fix: evidence

--- app/Http/Requests/AttendanceRequest.php
+++ app/Http/Requests/AttendanceRequest.php
@@ -14,8 +14,8 @@ class AttendanceRequest extends ApiRequest
     {
         return [
             'photo' => 'required|mimes:jpeg,png,jpg,svg|max:2048',
-            'latitude' => 'required|decimal:10,7',
-            'longitude' => 'required|decimal:10,7',
+            'latitude' => 'required|decimal',
+            'longitude' => 'required|decimal',
             'work_hour' => [
                 'required',
                 'regex:/^([01][0-9]|2[0-3]):([0-5][0-9])$/',
